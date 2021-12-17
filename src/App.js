308c303ce19119e567663256c223a702aa2c20bb
Shreyas <rasaikarshreyas21@gmail.com>
2021-12-17 16:42:07 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "./container/Header";
+import ProductDetail from "./container/ProductDetail";
+import ProductListing from "./container/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
